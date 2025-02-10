bfa5da7caa7d8ab0d64112cf6360ec5f88e64f3f
Santript Mehta <santriptmehta@gmail.com>
2025-02-11 01:18:21 +0530

    saved in mongodb

--- AgriGenie-Backend/src/main/java/com/blankSpace/AgriGenie/Repository/MessageRepository.java
+++ AgriGenie-Backend/src/main/java/com/blankSpace/AgriGenie/Repository/MessageRepository.java
@@ -1,4 +1,9 @@
 package com.blankSpace.AgriGenie.Repository;
 
-public interface MessageRepository {
+import com.blankSpace.AgriGenie.Entity.Message;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message, String> {
 }
