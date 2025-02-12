e0ef8b7804f6e2118722e5aea08fa09eb6e51678
Santript Mehta <santriptmehta@gmail.com>
2025-02-12 23:31:24 +0530

    Added App.js screen

--- gpt-frontend/src/main.jsx
+++ gpt-frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BrainAnimation from './utils/BrainAnimation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
+    <BrainAnimation/>
   </StrictMode>,
 )
